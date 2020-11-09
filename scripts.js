61cc68eae32da9a33fe872d96670f729c436e2ae
Manuel <manuel.serrano943@gmail.com>
2020-11-09 17:48:08 +0100

    Sketch working

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const wrapper = document.querySelector(".wrapper")
+
+    function createSketch() {
+
+        let grid = document.createElement("div");
+
+        grid.setAttribute("style", "display: grid;grid-template-columns: repeat(16,40px);grid-template-rows:repeat(16,40px);justify-content:center;:center;");
+
+        wrapper.appendChild(grid);
+
+        for (let i = 0; i < 256; i++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            grid.appendChild(square);
+        }
+    }
+
+    createSketch();
+
+
+    let squareDivs = document.querySelectorAll(".square");
+    let squareArray = [...squareDivs];
+    for (let i = 0; i < 256; i++) {
+        squareArray[i].addEventListener("mouseover", function () {
+            squareArray[i].setAttribute("style", "background-color: pink");
+        })
+    }
+
+})
+
